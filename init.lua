99d1d259da205c50152b583623a5ae7cb6ec2a06
archfan <33993466+archfan7411@users.noreply.github.com>
2019-02-22 17:58:03 -0500

    Fix a few bugs and add a few minor features

--- init.lua
+++ init.lua
@@ -1,21 +1,29 @@
+function string.startswith(str, sub)
+    return string.sub(str, 1, sub.length) == check
+end
+
 local prefix = ""
 minetest.register_on_sending_chat_messages(function(message)
-     if message then
-        local realprefix = prefix .. " "
-        if prefix == "" then
-            realprefix = ""
+    if prefix then 
+        if message and not string.startswith(message, "/") and not string.startswith(message, ".") then
+            local realprefix = prefix .. " "
+            if prefix == "" then
+                realprefix = ""
+            end
+            minetest.send_chat_message(realprefix..message)
         end
-        minetest.send_chat_message(realprefix..message)
-     end
-     return true
+        return true
+    end
+    return false
 end)
+
 minetest.register_chatcommand("chat", {
     description = "Sets command to pipe messages through - none to disable the feature.",
     func = function(param)
         if param ~= "" and string.sub(param, 1, 1) ~= "/" then
             param = "/" .. param
         end
-        prefix = param or ""
-        minetest.display_chat_message("Current prefix: " .. prefix)
+        prefix = string.trim(param) or ""
+        minetest.display_chat_message(minetest.colorize("#00ff00", "Current prefix: "..prefix.."\nIf you type 'Hello there', '"..prefix.." Hello there' will be sent."))
     end,
-})
+          
